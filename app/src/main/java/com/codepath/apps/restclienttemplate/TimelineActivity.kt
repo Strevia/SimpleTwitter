055bd30bd4970600b0ffe2f7c27996ab96ba5c86
Strevia <phillipnissen7@gmail.com>
2022-10-06 11:13:03 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -0,0 +1,114 @@
+package com.codepath.apps.restclienttemplate
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.codepath.apps.restclienttemplate.models.Tweet
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
+
+
+class TimelineActivity : AppCompatActivity() {
+
+    lateinit var client: TwitterClient
+    lateinit var rvTweets: RecyclerView
+    lateinit var adapter: TweetsAdapter
+    lateinit var swipeContainer: SwipeRefreshLayout
+    lateinit var scrollListener: EndlessRecyclerViewScrollListener
+
+
+    val tweets = ArrayList<Tweet>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+
+        client = TwitterApplication.getRestClient(this)
+
+        swipeContainer = findViewById(R.id .swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            Log.i("caren", "Refreshing timeline")
+            populateHomeTimeline()
+        }
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+            android.R.color.holo_green_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_red_light
+        )
+        rvTweets = findViewById<RecyclerView>(R.id.rvTweets)
+        adapter = TweetsAdapter(tweets)
+        val linearLayoutManager = LinearLayoutManager(this)
+        rvTweets.layoutManager = linearLayoutManager
+        rvTweets.adapter = adapter
+
+        scrollListener = object : EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadMoreData()
+            }
+        }
+        rvTweets.addOnScrollListener(scrollListener)
+
+        populateHomeTimeline()
+    }
+
+    private fun loadMoreData() {
+        var maxId = tweets[tweets.size-1].uid
+        client.getNextPageOfTweets(object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.i("Caren", "onFailure: $statusCode $response")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i("Caren", "onSuccess")
+                val jsonArray = json.jsonArray
+                try {
+                    val listOfNewTweets = Tweet.fromJsonArray(jsonArray)
+                    tweets.addAll(listOfNewTweets)
+                    adapter.notifyDataSetChanged()
+                    swipeContainer.setRefreshing(false)
+                } catch (e: JSONException){
+                    Log.e("Caren", "JSON Exception $e")
+                }
+            }
+
+        }, maxId)
+    }
+
+    fun populateHomeTimeline() {
+        client.getHomeTimeline(object: JsonHttpResponseHandler() {
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.i("Caren", "onFailure: $statusCode $response")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.i("Caren", "onSuccess")
+                val jsonArray = json.jsonArray
+                try {
+                    adapter.clear()
+                    val listOfNewTweets = Tweet.fromJsonArray(jsonArray)
+                    tweets.addAll(listOfNewTweets)
+                    adapter.notifyDataSetChanged()
+                    swipeContainer.setRefreshing(false)
+                } catch (e: JSONException) {
+                    Log.e("Caren", "JSON Exception $e")
+                }
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -0,0 +1,30 @@
+package com.codepath.apps.restclienttemplate.models
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+class Tweet {
+    var body: String = ""
+    var createdAt: String = ""
+    var user: User? = null
+    var uid: Long = 0
+    companion object {
+        fun fromJson(jsonObject: JSONObject): Tweet {
+            val tweet = Tweet()
+            tweet.body = jsonObject.getString("text")
+            tweet.createdAt = jsonObject.getString("created_at")
+            tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+            tweet.uid = jsonObject.getLong("id")
+            return tweet
+        }
+
+        fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
+            val tweets = ArrayList<Tweet>()
+            for (i in 0 until jsonArray.length()){
+                tweets.add(fromJson(jsonArray.getJSONObject(i)))
+            }
+            return tweets
+        }
+    }
+
+}
